4fc383c8b97002dc70ef11bfb364ce0f44f75c3c
eyemovic_tokunaga <tokunaga@eyemovic.com>
2022-12-29 20:37:57 +0900

    fix portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+import { FaTwitter, FaFacebook, FaGithub } from 'react-icons/fa';
+
+export const Header = () => {
+  return (
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
+            <h1 className="title-text">りきえる</h1>
+            <h3 className="title-text">Frontend Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* リンク先(href)は任意のURLを設定してください */}
+                <a href="https://twitter.com/">
+                  <FaTwitter color="Black" size="2rem" />
+                </a>
+              </li>
+							<li className="icon-link">
+                {/* リンク先(href)は任意のURLを設定してください */}
+                <a href="https://facebook.com/">
+                  <FaFacebook color="Black" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                {/* リンク先(href)は任意のURLを設定してください */}
+                <a href="https://github.com/">
+                  <FaGithub color="Black" size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
